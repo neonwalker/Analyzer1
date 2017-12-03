0e229fa65f9cfeb397807dc67ddd7dcc7abe743a
Neo <Neo@COMPUTER>
2017-12-03 18:10:45 +0400

    Changes

---
+++ Analyzer1/Analyzer1/MethodPropertyEvent/MethodParamsAnalyzer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Analyzer1.MethodPropertyEvent
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    class MethodParamsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "EA1008";
+        private static readonly string Title = "Parameter naming fault";
+        private static readonly string MessageFormat = @"Not lower camel case";
+        private static readonly string Description = "Parameter naming fault";
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var method = context.Node as MethodDeclarationSyntax;
+            var paramsList = method.ParameterList;
+            var parameters = paramsList.Parameters;
+            foreach (ParameterSyntax item in parameters)
+            {
+                if(item.Identifier.Text!=String.Empty)
+                {
+                    if((!char.IsLower(item.Identifier.Text[0])))
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(Rule, item.GetLocation()));
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Analyzer1/Analyzer1/MethodPropertyEvent/MethodParamsFixer.cs
@@ -0,0 +1,63 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.Immutable;
+//using System.Composition;
+//using System.Linq;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Microsoft.CodeAnalysis;
+//using Microsoft.CodeAnalysis.CodeFixes;
+//using Microsoft.CodeAnalysis.CodeActions;
+//using Microsoft.CodeAnalysis.CSharp;
+//using Microsoft.CodeAnalysis.CSharp.Syntax;
+//using Microsoft.CodeAnalysis.Rename;
+//using Microsoft.CodeAnalysis.Text;
+//using System.Globalization;
+//using System.Text;
+
+//namespace Analyzer1.MethodPropertyEvent
+//{
+//    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MethodParamsAnalyzer)), Shared]
+//    class MethodParamsFixer : CodeFixProvider
+//    {
+//        private const string title = "Parameter naming";
+
+//        public sealed override ImmutableArray<string> FixableDiagnosticIds
+//        {
+//            get { return ImmutableArray.Create(MethodParamsAnalyzer.DiagnosticId); }
+//        }
+
+//        public sealed override FixAllProvider GetFixAllProvider()
+//        {
+//            return WellKnownFixAllProviders.BatchFixer;
+//        }
+//        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+//        {
+//            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+//            var diagnostic = context.Diagnostics.First();
+//            var diagnosticSpan = diagnostic.Location.SourceSpan;
+//            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
+
+//            context.RegisterCodeFix(
+//                CodeAction.Create(
+//                    title: title,
+//                    createChangedSolution: c => RenameInterfaceAsync(context.Document, declaration, c),
+//                    equivalenceKey: title),
+//                diagnostic);
+//        }
+
+//        private async Task<Solution> RenameInterfaceAsync(Document document, MethodDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+//        {
+//            var identifierToken = typeDecl.ParameterList;
+//            var newName = identifierToken.Text.TitleCaseString();
+
+//            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+//            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+//            var originalSolution = document.Project.Solution;
+//            var optionSet = originalSolution.Workspace.Options;
+//            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+
+//            return newSolution;
+//        }
+//    }
+//}
